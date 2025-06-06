65f4f99b037be24f5f8d67149d31659b8be2e595
dmytroromAmasty <dmytro.romanenko@amasty.com>
2025-06-06 15:12:28 +0300

    initial app setup, OAuth 2.0 implemented, webhooks handling logic added

---
+++ app/routes/app._index.tsx
@@ -0,0 +1,47 @@
+import { useEffect } from "react";
+import type { ActionFunctionArgs, LoaderFunctionArgs } from "@remix-run/node";
+import { useFetcher } from "@remix-run/react";
+import {
+  Page,
+  Layout,
+  BlockStack,
+} from "@shopify/polaris";
+import { TitleBar, useAppBridge } from "@shopify/app-bridge-react";
+import { authenticate } from "../shopify.server";
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  await authenticate.admin(request);
+
+  return null;
+};
+
+export const action = async ({ request }: ActionFunctionArgs) => {
+  const { admin } = await authenticate.admin(request);
+  
+  return null;
+};
+
+export default function Index() {
+  const fetcher = useFetcher<typeof action>();
+
+  const shopify = useAppBridge();
+
+  useEffect(() => {
+    shopify.toast.show("Product created");
+  }, [shopify]);
+
+  const generateProduct = () => fetcher.submit({}, { method: "POST" });
+
+  return (
+    <Page>
+      <TitleBar title="Remix app template">
+        <button variant="primary" onClick={generateProduct}>
+          Generate a product
+        </button>
+      </TitleBar>
+      <BlockStack gap="500">
+        <Layout></Layout>
+      </BlockStack>
+    </Page>
+  );
+}
---
+++ app/routes/app.additional.tsx
@@ -0,0 +1,12 @@
+import {
+  Page,
+} from "@shopify/polaris";
+import { TitleBar } from "@shopify/app-bridge-react";
+
+export default function AdditionalPage() {
+  return (
+    <Page>
+      <TitleBar title="Additional page" />
+    </Page>
+  );
+}
