2c41e9448522e610809e8949a75ef780d9572baa
soloveichukAmasty <mariia.soloveichuk@amasty.com>
2025-07-24 18:16:35 +0300

    feat: added getting started page

--- app/routes/app._index.tsx
+++ app/routes/app._index.tsx
@@ -1,174 +1,124 @@
-import {useCallback, useEffect, useState} from "react";
-import {useFetcher, useLoaderData} from "@remix-run/react";
-import {BlockStack, Layout, Page,} from "@shopify/polaris";
-import ShopIntegrationForm from "../components/HomePage/ShopIntegrationForm";
-import ShopIntegrationCard from "../components/HomePage/ShopIntegrationCard";
-
-import type {ActionFunctionArgs, LoaderFunctionArgs} from "@remix-run/node";
-import type {FastEditorShopSettings} from "../types/fastEditor.types";
-import type {IntegrationActionData, IntegrationFormValues} from "../types/integration.types";
+import React from "react";
+import type {LoaderFunctionArgs} from "@remix-run/node";
+import {BlockStack, Box, Button, Card, Layout, List, Page, Text} from "@shopify/polaris";
 import {authenticate} from "../shopify.server";
+import {useLoaderData} from "@remix-run/react";
 import {billingRequire} from "../services/billing.server";
-import {
-  getFastEditorShopSettings,
-  parseFormData,
-  setupFastEditorIntegration,
-  validateFormData
-} from "../services/fastEditorFactory.server";
-import {createOrderMetafieldDefinition} from "../services/metafield.server";
 
 const ENDPOINT = "/app/_index";
 
-export const loader = async (
-  {request}: LoaderFunctionArgs
-): Promise<Response | FastEditorShopSettings> => {
+/**
+ * Loader for the Getting Started page.
+ */
+export const loader = async ({request}: LoaderFunctionArgs): Promise<string> => {
+  console.info(`[${ENDPOINT}] Getting Started Loader`);
+
   const {admin, billing, session} = await authenticate.admin(request);
   await billingRequire(admin, billing, session.shop);
 
-  try {
-    const shopSettings = await getFastEditorShopSettings(session.shop)
-
-    return {
-      fastEditorApiKey: shopSettings?.fastEditorApiKey ?? "",
-      fastEditorDomain: shopSettings?.fastEditorDomain ?? "",
-    };
-  } catch (error) {
-    const errorMessage = error instanceof Error ? error.message : String(error)
-    console.error(`[${ENDPOINT}] Loader error:`, errorMessage);
-    return new Response(errorMessage,
-      {status: 200}
-    );
-  }
-};
-
-export const action = async ({request}: ActionFunctionArgs): Promise<any> => {
-  const {admin, session} = await authenticate.admin(request);
-
-  try {
-    console.info(`[${ENDPOINT}] FastEditor API integration request for shop ${session.shop}`);
-    const {apiKey, apiDomain} = await parseFormData(request);
-    const errors = validateFormData(apiKey, apiDomain);
-
-    if (Object.keys(errors).length > 0) {
-      return new Response(JSON.stringify({
-          statusCode: 400,
-          statusText: "Validation errors",
-          body: {errors},
-          ok: false,
-        }),
-        {
-          status: 200,
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-    }
-
-    await setupFastEditorIntegration(admin, session.shop, apiKey, apiDomain);
-
-    await createOrderMetafieldDefinition(admin)
-
-    return new Response(JSON.stringify({
-        statusCode: 200,
-        statusText: "FastEditor integration is successful.",
-        ok: true
-      }),
-      {
-        status: 200,
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    );
-  } catch (error) {
-    const errorMessage = error instanceof Error ? error.message : String(error)
-    console.error(`[${ENDPOINT}] FastEditor integration failed.`, errorMessage);
-    return new Response(JSON.stringify({
-        statusCode: 500,
-        statusText: errorMessage,
-        ok: false
-      }),
-      {
-        status: 200,
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    );
-  }
+  return session.shop.replace(".myshopify.com", "")
 };
 
 const Index = () => {
-  const {fastEditorApiKey, fastEditorDomain} = useLoaderData<FastEditorShopSettings>()
-  const fetcher = useFetcher<IntegrationActionData>();
-  const [formValues, setFormValues] = useState<IntegrationFormValues>({
-    apiKey: fastEditorApiKey ?? "",
-    apiDomain: fastEditorDomain ?? "",
-  });
-  const [isApiKeyError, setApiKeyError] = useState<boolean>(false);
-  const [isApiDomainError, setApiDomainError] = useState<boolean>(false);
-  const [fastEditorError, setFastEditorError] = useState<boolean>(false);
-  const formErrors = fetcher.data?.body?.errors
-
-  useEffect(() => {
-    if (fetcher.state !== "idle" || !fetcher.data) return;
-
-    if (fetcher.data?.ok) {
-      shopify.toast.show(fetcher.data.statusText);
-    } else {
-      shopify.toast.show("Connection to FastEditor failed. Please check your API Key and Domain and try again.");
-    }
-
-    if (!fetcher.data?.ok) {
-      setFastEditorError(true);
-    } else {
-      setFastEditorError(false);
-    }
-
-    setApiKeyError(!!formErrors?.apiKey);
-    setApiDomainError(!!formErrors?.apiDomain);
-  }, [fetcher.state, fetcher.data]);
-
-  const handleChange = useCallback(
-    (field: keyof typeof formValues) => (value: string) => {
-      setFormValues(prev => ({...prev, [field]: value}));
-    }, []);
-
-  const handleSubmit = useCallback(async () => {
-    fetcher.submit(
-      {
-        apiKey: formValues.apiKey,
-        apiDomain: formValues.apiDomain,
-      },
-      {
-        method: "POST"
-      });
-  }, [formValues, fetcher]);
+  const shopName = useLoaderData<string>()
 
   return (
-    <Page fullWidth>
-      <BlockStack gap="500">
+    <Page title="Getting Started">
+      <Box paddingBlockEnd="500">
         <Layout>
           <Layout.Section>
-            <ShopIntegrationCard
-              fastEditorApiKey={fastEditorApiKey}
-              fastEditorDomain={fastEditorDomain}
-            >
-              <ShopIntegrationForm
-                handleChange={handleChange}
-                handleSubmit={handleSubmit}
-                formValues={formValues}
-                isApiKeyError={isApiKeyError}
-                isApiDomainError={isApiDomainError}
-                errors={formErrors}
-                fastEditorError={fastEditorError}
-              />
-            </ShopIntegrationCard>
+            <Card>
+              <BlockStack gap="200">
+                <Text as="h2" variant="headingMd">
+                  FastEditor Integration
+                </Text>
+                <Text as="p" tone="subdued" variant="bodyMd">
+                  Before using FastEditor, connect your store to the platform via the Settings page.
+                </Text>
+                <Text as="span">
+                  <Button url="/app/settings">Go to Settings</Button>
+                </Text>
+              </BlockStack>
+            </Card>
+          </Layout.Section>
+
+          <Layout.Section>
+            <Card>
+              <BlockStack gap="200">
+                <Text as="h2" variant="headingMd">
+                  Customization Button
+                </Text>
+                <Text as="p" tone="subdued" variant="bodyMd">
+                  Add a customization button to your product page template and style it to match
+                  your
+                  theme.
+                </Text>
+                <Text as="span">
+                  <Button
+                    url={`https://admin.shopify.com/store/${shopName}/themes/current/editor`}
+                    target="_blank"
+                  >
+                    Customize Appearance
+                  </Button>
+                </Text>
+              </BlockStack>
+            </Card>
+          </Layout.Section>
+
+          <Layout.Section>
+            <Card>
+              <BlockStack gap="200">
+                <Text as="h2" variant="headingMd">
+                  Product Setup
+                </Text>
+                <Text as="p" tone="subdued" variant="bodyMd">
+                  To link a Shopify product with FastEditor, follow these steps:
+                </Text>
+                <List>
+                  <List.Item>Create or select a product in Shopify.</List.Item>
+                  <List.Item>Go to the FastEditor platform.</List.Item>
+                  <List.Item>Find the corresponding product.</List.Item>
+                  <List.Item>Click <b>Configurations</b>.</List.Item>
+                  <List.Item>You’ll land on the product details page.</List.Item>
+                  <List.Item>Select the filters you want to use.</List.Item>
+                  <List.Item>Click <b>SmartLink payload</b>.</List.Item>
+                  <List.Item>Copy the <b>SKU</b>.</List.Item>
+                  <List.Item>Return to the product settings in Shopify.</List.Item>
+                  <List.Item>
+                    <b>If the product has no variants:</b> paste the SKU into the <i>SKU (Stock
+                    Keeping Unit)</i> field in the Inventory section and save your changes.
+                  </List.Item>
+                  <List.Item>
+                    <b>If the product has variants:</b> open the specific variant, paste the SKU
+                    into the <i>SKU (Stock Keeping Unit)</i> field in the Inventory section and
+                    save your changes.
+                  </List.Item>
+                </List>
+
+                <Text as="p" tone="subdued" variant="bodyMd">
+                  To display the customization button on the product page:
+                </Text>
+                <List>
+                  <List.Item>Open the product you want to customize.</List.Item>
+                  <List.Item>Scroll to the <b>Tags</b> section.</List.Item>
+                  <List.Item>Add the <code>fasteditor</code> tag.</List.Item>
+                </List>
+
+                <Text as="span">
+                  <Button
+                    url={`https://admin.shopify.com/store/${shopName}/products?selectedView=all`}
+                    target="_top"
+                  >
+                    Go to Products
+                  </Button>
+                </Text>
+              </BlockStack>
+            </Card>
           </Layout.Section>
         </Layout>
-      </BlockStack>
+      </Box>
     </Page>
   );
-}
+};
+
 export default Index;
