a27d778832c2019331d647a05da850b26785cbe1
soloveichukAmasty <mariia.soloveichuk@amasty.com>
2025-09-01 11:45:19 +0300

    feat: added GET /cron/usage-billing and cron/currency-rates/update

--- app/routes/cron.usage-billing.tsx
+++ app/routes/cron.usage-billing.tsx
@@ -1,19 +1,23 @@
 import {processMonthlyUsageBilling} from "../services/usageBilling.server";
-import type {LoaderFunctionArgs} from "@remix-run/node";
-import {loaderMethodNotAllowed} from "../services/app.server";
 
 const ENDPOINT = "cron/usage-billing"
 
 /**
  * GET requests are not allowed on this endpoint.
  */
-export const loader = async ({request}: LoaderFunctionArgs): Promise<void> => {
-  loaderMethodNotAllowed({request, endpoint: ENDPOINT})
+export const loader = async (): Promise<Response> => {
+  try {
+    await processMonthlyUsageBilling()
+    console.info(`[${ENDPOINT}] Monthly usage billing completed successfully.`);
+
+    return new Response("Billing run completed.", {status: 200});
+  } catch (error: any) {
+    console.error(`[${ENDPOINT}] Error during billing run:`, error?.message ?? error);
+    return new Response("Internal Server Error", {status: 500});
+  }
 };
 
 export const action = async (): Promise<Response> => {
-  console.info(`[${ENDPOINT}] Billing job started...`);
-
   try {
     await processMonthlyUsageBilling()
     console.info(`[${ENDPOINT}] Monthly usage billing completed successfully.`);
