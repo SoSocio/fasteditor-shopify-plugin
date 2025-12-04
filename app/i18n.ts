f8faacc52c3a3fbc3e81299d7f29cce195ba50c3
soloveichukAmasty <mariia.soloveichuk@amasty.com>
2025-12-04 10:40:28 +0200

    feat: added Dutch, Swedish, Spanish, Portuguese, Polish, Italian, Danish languages

--- app/i18n.ts
+++ app/i18n.ts
@@ -1,5 +1,5 @@
 export default {
-  supportedLngs: ['en', 'fr', 'de'],
+  supportedLngs: ['en', 'de', 'fr', 'nl', 'sv', 'es', 'pt', 'pl', 'it', 'da'],
   fallbackLng: 'en',
   defaultNS: 'common',
 };
