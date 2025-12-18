4b5586b07a05db7835cb240d2d53072ef78c9b49
soloveichukAmasty <mariia.soloveichuk@amasty.com>
2025-12-18 12:04:57 +0200

    feat: added new image and video

--- app/routes/app._index.tsx
+++ app/routes/app._index.tsx
@@ -138,7 +138,19 @@ const Index = () => {
                 <Text as="h2" variant="headingMd">
                   {t("getting-started-page.customization-button.title")}
                 </Text>
-                <video style={{ width: "100%", height: "100%", borderRadius: "6px" }} autoPlay muted loop poster="customize-button.png">
+                <video
+                  style={{
+                    width: "100%",
+                    height: "100%",
+                    borderRadius: "6px",
+                    objectFit: "cover",
+                    aspectRatio: "16 / 9"
+                  }}
+                  autoPlay
+                  muted
+                  loop
+                  poster="customize-button.png"
+                >
                   <source src="customize-button.mp4" type="video/mp4"/>
                 </video>
                 <Text as="p" variant="bodyMd">
@@ -159,7 +171,18 @@ const Index = () => {
                 <Text as="h2" variant="headingMd">
                   {t("getting-started-page.sticky-bar.title")}
                 </Text>
-                <video style={{ width: "100%", height: "100%", borderRadius: "6px" }} autoPlay muted loop poster="sticky-bar.png">
+                <video
+                  style={{
+                    width: "100%",
+                    height: "100%",
+                    borderRadius: "6px",
+                    objectFit: "cover",
+                    aspectRatio: "16 / 9"
+                  }}
+                  autoPlay
+                  muted
+                  loop poster="sticky-bar.png"
+                >
                   <source src="sticky-bar.mp4" type="video/mp4"/>
                 </video>
                 <Text as="p" variant="bodyMd">
